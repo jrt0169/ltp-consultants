76ccdf891cc8d237916db0150cfc9ba9e373bd8a
jrt0169 <106993077+jrt0169@users.noreply.github.com>
2026-05-02 21:03:06 -0500

    fix: remove fake client logo trust bar (Logo1)

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 import { Navbar10 } from "@/components/shared/Navbar10";
 import { Header145 } from "@/components/home/Header145";
-import { Logo1 } from "@/components/home/Logo1";
 import { Layout237 } from "@/components/home/Layout237";
 import { Layout4 } from "@/components/home/Layout4";
 import { Layout239 } from "@/components/home/Layout239";
@@ -11,7 +10,6 @@ export default function HomePage() {
     <div>
       <Navbar10 />
       <Header145 />
-      <Logo1 />
       <Layout237 />
       <Layout4 />
       <Layout239 />

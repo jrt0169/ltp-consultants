ce29327446d79fb3c47c8323cc271cfe9073d852
jrt0169 <106993077+jrt0169@users.noreply.github.com>
2026-05-04 16:02:27 -0500

    fix: wrap -> in JSX expression to prevent parse error

--- components/services/Layout356.jsx
+++ components/services/Layout356.jsx
@@ -41,7 +41,7 @@ export function Layout356() {
                           size="link"
                           iconRight={<RxChevronRight />}
                         >
-                          ->
+                          {'->'}
                         </Button>
                       </div>
                     </div>
@@ -93,7 +93,7 @@ export function Layout356() {
                           size="link"
                           iconRight={<RxChevronRight />}
                         >
-                          ->
+                          {'->'}
                         </Button>
                       </div>
                     </div>
@@ -143,7 +143,7 @@ export function Layout356() {
                           size="link"
                           iconRight={<RxChevronRight />}
                         >
-                          ->
+                          {'->'}
                         </Button>
                       </div>
                     </div>
